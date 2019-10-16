b3c0247b40620fb2a56a7cdb2cac623b3212a821
tukno <oliveiradavid007@gmail.com>
2019-10-15 23:36:44 -0400

    state and imutability - To update component state it is necesary to use the method useState(); - In a class defined component it is necessary to write it as an arrow function.

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 
 class TechList extends Component {
-  state = { techs: ['Node.js', 'ReactJS', 'React Native'] };
+  state = {
+    techs: ['Node.js', 'ReactJS', 'React Native'],
+    newTech: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+    });
+  };
 
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>React</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
